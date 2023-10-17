fe31826b4225bce3f42f62f9bfdaacf3b78d3b88
Clayswork <144185410+Clayswork@users.noreply.github.com>
2023-10-16 21:10:53 -0600

    Add files via upload

---
+++ Python Unit Testing/calc.py
@@ -0,0 +1,21 @@
+def add(x, y):
+
+
+    return x + y
+
+
+def subtract(x, y):
+
+    return x - y
+
+
+def multiply(x, y):
+
+    return x * y
+
+
+def dvide(x, y):
+
+    if y == 0:
+        raise ValueError('Can not divide by zero!')
+    return x / y
